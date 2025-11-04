055148bb65a90ffc25b21a68de9d01f8c36dd526
faustoche <faustine.crocq@gmail.com>
2025-11-04 15:04:52 +0100

    correction du prefix dans gateway

--- srcs/back/gateway/scripts/index.ts
+++ srcs/back/gateway/scripts/index.ts
@@ -8,25 +8,25 @@ const fastify = Fastify({ logger: true });
 fastify.register(fastifyProxy, {
 	upstream: 'http://auth:3001', // adresse interne du réseau du docker
 	prefix: '/api/auth', // toutes les requetes api/auth iront au service auth
-	rewritePrefix: '/api/auth' // on retire le prefixe avant de l'envoyer un service
+	rewritePrefix: '' // on retire le prefixe avant de l'envoyer un service
 });
 
 fastify.register(fastifyProxy, {
 	upstream: 'http://chat:3002', // adresse interne du réseau du docker
 	prefix: '/api/chat', // toutes les requetes api/chat iront au service chat
-	rewritePrefix: '/api/chat' // on retire le prefixe avant de l'envoyer un service
+	rewritePrefix: '' // on retire le prefixe avant de l'envoyer un service
 });
 
 fastify.register(fastifyProxy, {
 	upstream: 'http://game:3003', // adresse interne du réseau du docker
 	prefix: '/api/game', // toutes les requetes api/game iront au service game
-	rewritePrefix: '/api/game' // on retire le prefixe avant de l'envoyer un service
+	rewritePrefix: '' // on retire le prefixe avant de l'envoyer un service
 });
 
 fastify.register(fastifyProxy, {
 	upstream: 'http://user:3004', // adresse interne du réseau du docker
 	prefix: '/api/user', // toutes les requetes api/user iront au service user
-	rewritePrefix: '/api/user' // on retire le prefixe avant de l'envoyer un service
+	rewritePrefix: '' // on retire le prefixe avant de l'envoyer un service
 });
 
 // route de test
