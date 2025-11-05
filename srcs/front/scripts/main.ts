aba3f21fa9defba9b58f62c39d6f2d01e2bbcd33
Anne-Charlotte Vandermolen <anvander@f6r13s9.clusters.42paris.fr>
2025-11-05 12:13:16 +0100

    intégration des nouvelles pages front

---
+++ srcs/front/scripts/main.ts
@@ -0,0 +1,9 @@
+// Message à afficher dans la console du navigateur
+console.log("TypeScript est prêt et fonctionnel.");
+
+// Sélectionne la div app de l'index.html
+const appElement = document.getElementById('app');
+
+// Test l'interaction avec le DOM
+if (appElement)
+	appElement.innerHTML += '<h2 class="text-green-500 p-4">Test TypeScript: Injecté avec succès! </h2>';
