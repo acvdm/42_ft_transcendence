c19f4f384cc76ced3788e6bbc636a02fd1a1e115
faustoche <faustine.crocq@gmail.com>
2025-11-07 09:33:27 +0100

    trying to get tailwind to update in html

--- srcs/front/scripts/pages/HomePage.ts
+++ srcs/front/scripts/pages/HomePage.ts
@@ -1,13 +1,13 @@
 // on va exportrter une fonction qui renvoie du html 
 export function HomePage(): string {
 	return `
-		<div class="p-8">
+		<div class="w-screen h-[200px] bg-cover bg-center bg-no-repeat bg-linear-to-t from-cyan-300 to-zinc-100" style="background: linear-gradient(white, #cceffc); background-image: url(https://wlm.vercel.app/assets/background/background.jpg); background-size: cover;">
 			<h1 class="text-4xl font-bold text-blue-400 mb-4">
 				This is Transcendence 🚧
 			</h1>
 			<p class="text-lg text-gray-300">
 				Work in progress....
 			</p>
-			</div>
+		</div>
 	`;
 };
