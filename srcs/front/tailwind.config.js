36ad44aa43168a0a4a4851eab2b71d7199a1c67a
faustoche <faustine.crocq@gmail.com>
2025-11-10 21:34:51 +0100

    fixed page layout

--- srcs/front/tailwind.config.js
+++ srcs/front/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// je dis a tailwind de scanner ces fichiers
-	content: [
-		"./public/index.html", // page html SPA principale
-		"./scripts/**/*.ts" // les fichiers typescripts
-	],
-	theme: {
-		extend: {},
-		colors: {
-			msn_blue: "#43B5EB"
-		}
-		
-	},
-	plugins: [],
+    // je dis a tailwind de scanner ces fichiers
+    content: [
+        "./public/index.html", // page html SPA principale
+        "./scripts/**/*.ts" // les fichiers typescripts
+    ],
+    theme: {
+        extend: { // <-- On ouvre "extend"
+            colors: { // <-- On met "colors" A L'INTERIEUR
+                msn_blue: "#43B5EB"
+            }
+        } // <-- On ferme "extend"
+    },
+    plugins: [],
 }
