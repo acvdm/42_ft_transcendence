421b36048aa5c1192022efcc4a77cf4152610b79
faustoche <faustine.crocq@gmail.com>
2025-11-25 22:00:19 +0100

    persistence de la session de connexion!

--- srcs/front/scripts/pages/RegisterPage.ts
+++ srcs/front/scripts/pages/RegisterPage.ts
@@ -87,6 +87,8 @@ function handleRegister() {
 				if (data.user_id) {
 					localStorage.setItem('userId', data.user_id.toString());
 				}
+
+				localStorage.setItem('username', alias);
 				
 				if (data.access_token) // on sauvegarde le token si necessaire
 					localStorage.setItem('accessToken', data.access_token);
