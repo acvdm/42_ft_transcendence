3aa6bb2c0ffb296a179c8c5eaec5b01e8cf9d7c1
Cassandre Maubert <cmaubert@f6r13s11.clusters.42paris.fr>
2026-01-19 13:41:53 +0100

    suppression commentaires

--- srcs/back/auth/src/utils/mailer.ts
+++ srcs/back/auth/src/utils/mailer.ts
@@ -33,8 +33,3 @@ export async function send2FAEmail(toEmail: string, code: string) : Promise<void
 
 }
 
-// false -> ne pas commencer la connexion en SSL direct, 
-// le serveur (serveur mail de google) qui supporte le cryptage 
-// l'active au moment de l'envoi du mot de passe
-// 587 : standard moderne recommande pour l'envoi de mails par de appli ("Soumission de message")
-
